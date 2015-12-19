62c6166e7df2ccd3bf33b46c3e9536702b4d3971
DongNan <dongyh@126.com>
2015-12-19 23:20:03 +0800

    注释修改

--- src/linktool/setup.php
+++ src/linktool/setup.php
@@ -13,11 +13,11 @@
 defined('LT_MEMORY_ON') || define('LT_MEMORY_ON', function_exists('memory_get_usage'));
 
 //验证条件
-/** 存在字段就验证（默认） */
+/** 字段存在则验证（默认） */
 define('LT_VALIDATE_EXISTS', 0);
 /** 必须验证 */
 define('LT_VALIDATE_MUST', 1);
-/** 值不为空的时候验证 */
+/** 值不为空则验证 */
 define('LT_VALIDATE_VALUE', 2);
 
 //触发验证事件（可选）
