a5afa5136d21a8c8edeaf3e01c17278cd0a475f6
dongnan <dongyh@126.com>
2017-07-08 09:47:13 +0800

    增加方法:根据权重获取数组元素

--- src/linktool/ArrayTool.php
+++ src/linktool/ArrayTool.php
@@ -34,4 +34,28 @@ class ArrayTool {
         return $data;
     }
 
+    /**
+     * 根据权重获取数组元素
+     * @param array $array
+     * @param string $weightKey
+     * @return array
+     */
+    static public function weightRandom($array, $weightKey = 'weight') {
+        $count = 0;
+        foreach ($array as $item) {
+            $count += isset($item[$weightKey]) ? $item[$weightKey] : 1;
+        }
+        $minNum    = 0;
+        $maxNum    = 0;
+        $randomNum = random_int($minNum, $count);
+        foreach ($array as $item) {
+            $maxNum += isset($item[$weightKey]) ? $item[$weightKey] : 1;
+            if ($minNum < $randomNum && $randomNum <= $maxNum) {
+                return $item;
+            }
+            $minNum += isset($item[$weightKey]) ? $item[$weightKey] : 1;
+        }
+        return $array[array_rand($array)];
+    }
+
 }
